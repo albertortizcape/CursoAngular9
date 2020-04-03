ddccafc28af9de7641f6fa47401fa51ce0a3f441
aortiz <aortiz@hiberus.com>
2020-04-03 17:36:50 +0200

    Gracias a un compañero se resuelve el problema que no me dejaba pintar el titulo y el autor del libro.

--- src/app/detallelibro/detallelibro.component.ts
+++ src/app/detallelibro/detallelibro.component.ts
@@ -10,7 +10,7 @@ export class DetallelibroComponent implements OnInit {
 
   libros: Array<object>;
   libroId: number;
-  libroClick: object;
+  libroClick: any;
 
   constructor(private rutausuario: ActivatedRoute) { 
 
